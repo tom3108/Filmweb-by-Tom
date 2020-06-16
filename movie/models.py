93a2b539b9bf70946db9127aee10ab9a297d3233
tom3108 <t.madziara@gmail.com>
2020-06-16 23:24:47 +0200

    ManyToManyField

--- movie/models.py
+++ movie/models.py
@@ -43,3 +43,8 @@ class Rate(models.Model):
 	stars = models.PositiveSmallIntegerField(default=5)
 	film = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
+class Actor(models.Model):
+	name = models.CharField(max_length=30)
+	surname = models.CharField(max_length=30)
+	films = models.ManyToManyField(Movie)
+
